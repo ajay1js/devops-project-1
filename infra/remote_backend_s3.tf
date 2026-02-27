4113d9872d625a80606e1e9c9caf3fe9ea8ae75e
ajay1js <111346704+ajay1js@users.noreply.github.com>
2026-02-28 05:03:20 +0530

    Changed s3 bucket name

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
-    key    = "devops-project-1/terraform.tfstate"
+    bucket = "data-engineer-practise-ajayl12"
+    key    = "dev-ops-project-2/terraform.tfstate"
     region = "eu-central-1"
   }
 }
